aed18260e5b3cc2e2163f22717b09694e238de4f
oxSaturn <oxSaturn@proton.me>
2024-12-15 18:58:45 +0800

    chores

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,6 @@ import react from '@astrojs/react';
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), react()],
-  base: '/profiles'
+  base: '/profiles',
+  outDir: 'profiles'
 });
